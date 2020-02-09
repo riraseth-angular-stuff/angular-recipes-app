0d153e2a45a99890517d81b72e27eecdc94dfb19
Łukasz Zabuski <lukasz@biuro.bcweb.pl>
2020-02-09 20:06:44 +0100

    first commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.scss']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test recipe', 'This is a test', 'https://www.wellplated.com/wp-content/uploads/2017/12/Hoppin-John-recipe.jpg'),
+    // tslint:disable-next-line:max-line-length
+    new Recipe('Test Recipe 2', 'this is a test desc 2', 'https://keyassets-p2.timeincuk.net/wp/prod/wp-content/uploads/sites/53/2017/12/Vegetable-stew-recipe-920x605.jpg')
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
